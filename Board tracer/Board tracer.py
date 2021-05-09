ce328e5a36ec079f05364223ceee564b4d6af470
Rafael Castro <raphaeldavant@hotmail.com>
2021-05-09 19:52:07 -0300

    Board tracer refactored

--- Board tracer/Board tracer.py
+++ Board tracer/Board tracer.py
@@ -3,14 +3,15 @@ model2 = '|   |'
 model3 = '   |'
 
 def draw_board(columns,lines):
-	
-	print(model1*columns)
-	print(model2+model3*(columns-1))
+
 	print(model1*columns)
 
-	for i in range(lines-1):
+	for i in range(lines):
 		print(model2+model3*(columns-1))
 		print(model1*columns)
 
 
-draw_board(5,5)
+nColumns = int(input("How many columns?"))
+nLines = int(input("How many lines?"))
+
+draw_board(nColumns, nLines)
